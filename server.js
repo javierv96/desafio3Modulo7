881c54eab0d94246c0716522fc3f39f2857e05ec
Javier Vergara <ja.vergarac96@gmail.com>
2024-05-05 21:46:12 -0400

    Server.js

--- server.js
+++ server.js
@@ -1,21 +1,31 @@
+// Modulos requeridos
 const express = require('express');
 const app = express();
-
 const querys = require('./consultas/query.js');
 const errors = require('./errors/handleErrors');
 
+// Middleware para parsear el cuerpo de las solicitudes en formato JSON
 app.use(express.json());
 
+// Puerto en el que se ejecutara el servidor
 const PORT = process.env.SV_PORT || 3000;
 
+// Ruta principal que sirve el archivo index.html
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/index.html")
+    res.sendFile(__dirname + "/index.html") //cambiar archivo a Myindex.html para utilizar la version con mensajes del servidor.
 })
 
 // Variables globales de index.js
 let status = "";
 let message = "";
 
+// Tonalidades musicales validas
+const tonosValidos = [
+    "do", "do#", "re", "re#", "mi", "fa", "fa#", "sol", "sol#", "la", "la#", "si",
+    "reb", "mib", "solb", "lab", "sib"
+];
+
+// Ruta para obtener todas las canciones
 app.get('/canciones', async (req, res) => {
     try {
 
@@ -24,6 +34,7 @@ app.get('/canciones', async (req, res) => {
         res.json(canciones);
 
     } catch (err) {
+        // Manejo de errores
         console.log("Error General: ", err)
         const final = errors(err.code, status, message);
         console.log("Codigo de Error: ", final.code);
@@ -33,24 +44,34 @@ app.get('/canciones', async (req, res) => {
     }
 });
 
+// Ruta para agregar una nueva cancion
 app.post('/cancion', async (req, res) => {
     const { titulo, artista, tono } = req.body;
 
     try {
+
+        // En caso de no ingresar cualquiera de los campos se enviara un mensaje por servidor
         if (!titulo || !artista || !tono) {
             return res.status(400).json({ error: 'Por favor, proporciona titulo, artista y tono' });
+
         }
 
-        const agregarCancion = querys.agregar(titulo, artista, tono);
+        // En caso de ingresar un tono no valido se enviara un mensaje por servidor
+        const tonoMinusculas = tono.toLowerCase();
+        if (!tonosValidos.includes(tonoMinusculas)) {
+            return res.status(400).json({ error: 'Por favor, proporciona un tono valido.' })
+        }
 
-        res.status(201).json(
-            {
-                message: 'Cancion agregada correctamente.',
-                cancion: agregarCancion
-            }
-        );
+        const agregarCancion = querys.agregar(titulo, artista, tonoMinusculas);
+
+        // En caso de exito al agregar la cancion se enviara un mensaje por servidor
+        res.status(201).json({
+            message: 'Cancion agregada correctamente.',
+            cancion: agregarCancion
+        });
 
     } catch (err) {
+        // Manejo de errores
         console.log("Error General: ", err)
         const final = errors(err.code, status, message);
         console.log("Codigo de Error: ", final.code);
@@ -61,36 +82,44 @@ app.post('/cancion', async (req, res) => {
 
 });
 
+// Ruta para actualizar una cancion existente
 app.put('/cancion/:id', async (req, res) => {
     const { id } = req.params;
     const { titulo, artista, tono } = req.body;
 
     try {
 
+        // En caso de no ingresar cualquiera de los campos se enviara un mensaje por servidor
         if (!titulo || !artista || !tono) {
             return res.status(400).json({ error: 'Por favor, proporciona titulo, artista y tono para actualizar la canción.' });
         }
 
-        await querys.modificar(id, titulo, artista, tono);
+         // En caso de ingresar un tono no valido se enviara un mensaje por servidor
+        const tonoMinusculas = tono.toLowerCase();
+        if (!tonosValidos.includes(tonoMinusculas)) {
+            return res.status(400).json({ error: 'Por favor, proporciona un tono valido.' });
+        }
+
+        await querys.modificar(id, titulo, artista, tonoMinusculas);
 
         const cancionActualizada = await querys.consultar(id);
 
+        // En caso de exito al actualizar la cancion se enviara un mensaje por servidor
         if (cancionActualizada.length > 0) {
-            res.json(
-                {
-                    message: 'Canción actualizada correctamente.',
-                    cancion: cancionActualizada[0]
-                }
-            );
+            res.json({
+                message: 'Canción actualizada correctamente.',
+                cancion: cancionActualizada[0]
+            });
+
+        // En caso de error al actualizar la cancion se enviara un mensaje por servidor (Esto puede ocurrir si se realizan pruebas por thunderclient)
         } else {
-            res.status(400).json(
-                {
-                    message: "El registro con id: " + id + " no existe."
-                }
-            )
+            res.status(400).json({
+                message: "El registro con id: " + id + " no existe."
+            });
         }
 
     } catch (err) {
+        // Manejo de errores
         console.log("Error General: ", err)
         const final = errors(err.code, status, message);
         console.log("Codigo de Error: ", final.code);
@@ -100,6 +129,7 @@ app.put('/cancion/:id', async (req, res) => {
     }
 });
 
+// Ruta para eliminar una cancion
 app.delete('/cancion', async (req, res) => {
     const { id } = req.query;
     try {
@@ -122,11 +152,12 @@ app.delete('/cancion', async (req, res) => {
     }
 });
 
-app.listen(PORT, () => {
-    console.log(`Servidor escuchando en el puerto ${PORT}`);
-});
-
 // Middleware para manejar rutas no encontradas
 app.use((req, res) => {
     res.send('Esta página no existe...');
+});
+
+// Iniciar el servidor en el puerto especificado
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
